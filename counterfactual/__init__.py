e492c93071903710e965456899c71cd126c8bd52
rzfzr <capitao.rafa@gmail.com>
2026-02-23 14:08:27 +0100

    Add counterfactual module to Sphinx API documentation

---
+++ counterfactual/__init__.py
@@ -0,0 +1,5 @@
+"""Counterfactual explanation module for DPG.
+
+Provides counterfactual generation, constraint extraction, scoring, and
+visualisation on top of Decision Predicate Graph models.
+"""
